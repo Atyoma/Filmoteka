12afcb9516e49ff3c92c6172a507c58e1a7e2990
Artem Kravchenko <atyoma.kravchenko@gmail.com>
2022-01-29 22:15:54 +0200

    add modal-window and html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './sass/main.scss';
+
+const refs = {
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+};
+
+console.log(refs.openModalBtn);
+console.log(refs.closeModalBtn);
+console.log(refs.modal);
+
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  console.log('click');
+  refs.modal.classList.toggle('is-hidden');
+}
